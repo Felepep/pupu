82eceb64171b34f08008393413d8f9f7704bba78
Felepep <61751816+Felepep@users.noreply.github.com>
2020-10-11 12:37:25 -0300

    Update vvvv.py

--- vvvv.py
+++ vvvv.py
@@ -11,11 +11,10 @@ html = acesso_url.text
 num_pages = int(re.findall('<span class="pages">Pages(.+?):</span>', html)[0].replace('(', '').replace(')', ''))
 id = re.findall('action=profile&amp;uid=(.+?)">', html)  # o primeiro id que aparece é o seu
 
-if num_pages > 1:
-    for i in range(0, num_pages):
-        pids = re.findall('#pid(.+?)"', html)
-        for j in range(0, len(pids)):
-            data = {'action': 'xem_fast_rep', 'uid': id[1], 'pid': pids[j], 'reputation': -1}
-            hezbollah = requests.post(url, cookies=cookies, data=data, headers=headers)
-        new_url = requests.get(info_url + '&page={}'.format(i + 2))
-        html = new_url.txt
+for i in range(0, num_pages):
+    pids = re.findall('#pid(.+?)"', html)
+    for j in range(0, len(pids)):
+        data = {'action': 'xem_fast_rep', 'uid': id[1], 'pid': pids[j], 'reputation': -1}
+        hezbollah = requests.post(url, cookies=cookies, data=data, headers=headers)
+    new_url = requests.get(info_url + '&page={}'.format(i + 2))
+    html = new_url.txt
